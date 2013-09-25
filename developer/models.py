7eb8d5b76bc3691655c3c49b4dc46603f5697bf2
Barbara Shaurette <barbara.shaurette@gmail.com>
2013-09-24 19:36:56 -0500

    fix developer signup handling

--- developer/models.py
+++ developer/models.py
@@ -14,10 +14,12 @@ class Developer(models.Model):
     name = models.CharField(max_length=200, blank=False)
     email = models.CharField(max_length=200, blank=False, unique=True,
             verbose_name='Email Address')
-    phone = models.CharField(max_length=200)
     bio = models.CharField(max_length=200, blank=False,
             help_text='What languages do you work in?  What are your areas of \
             programming expertise?')
     availability = models.CharField(max_length=1, blank=False,
             choices=AVAILABILITY_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return u"%s" % (self.name)
--- developer/views.py
+++ developer/views.py
@@ -14,7 +14,22 @@ def developers(request):
 
     context = {}
     context["content"] = Description.objects.get(publish=True, slug='developers-austin-csta-volunteer-program-sign-ups')
-    context["form"] = DeveloperForm()
+
+    if request.method == 'POST':
+        form = DeveloperForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+            context["thankyou"] = Description.objects.get(publish=True, slug='thank-you-for-signing-up')
+            return render_to_response("developer_signup_success.html", context,
+                    context_instance=RequestContext(request))
+        else:
+            print form.is_valid()
+            print form.errors
+    else:
+        form = DeveloperForm()
+
+    context["form"] = form
 
     return render_to_response(template_name, context,
             context_instance=RequestContext(request))
